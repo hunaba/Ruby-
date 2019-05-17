c0127616e430dfef947a29c3e51aa7b3025fa70b
iku <abadeer.hunson@yandex.ru>
2019-05-17 17:25:33 +0200

    exo19

---
+++ TestRuby/exo19.rb
@@ -0,0 +1,19 @@
+mail = []
+
+50.times do |i|
+	if i<9 
+		adresse = "jean.dupont.0"+((i+1).to_s)+"@email.fr" 
+		puts adresse
+		mail << adresse
+	else
+		adresse = "jean.dupont."+((i+1).to_s)+"@email.fr"
+		puts adresse
+		mail << adresse
+	end
+end
+
+(mail.size).times do |i|
+	if !i.even?
+		puts mail[i]
+	end
+end
