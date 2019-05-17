b4e7317bba46f4f6fb754f1f09d429211558631e
iku <abadeer.hunson@yandex.ru>
2019-05-17 12:12:07 +0200

    exo7c explications

---
+++ TestRuby/exo07c.rb
@@ -0,0 +1,13 @@
+user_name = gets.chomp
+puts user_name
+
+#ça ouvre juste une ligne pour que tu tapes qqch
+#ça affiche ce que tu as tapé
+
+#donc:
+
+#K
+#K
+
+
+#user_name = gets.chomp te permet d'entrée tes données utilisateurs (gets c'est la methode)/ user_name devient le variable de ton nom / les programmes demandent mon nom et avec quelques mots en plus ils seront en mesure de me saluer et pas seulement répéter le nom;) 
