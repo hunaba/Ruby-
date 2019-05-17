74e75f6dc38e2ce94e71e35a310e1c1848d8d3ec
iku <abadeer.hunson@yandex.ru>
2019-05-17 13:15:51 +0200

    exo10.rb

---
+++ TestRuby/exo10.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton année de naissance? "
+annee_naissance = gets.chomp
+age= 2017 - annee_naissance.to_i
+puts "Alors ton âge en 2017 était de...hummm... #{age} ans. You're old."
+
