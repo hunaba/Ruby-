db6c777cc685bb3f477757b947e374a27e7248d7
iku <abadeer.hunson@yandex.ru>
2019-05-17 17:36:30 +0200

    exo5

---
+++ TestRuby/exo05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #puts permet d'afficher
+puts "Travail : #{10 * 5 * 11}" # les symboles #{} permettent de concatener un entier dans un string
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # l'asterisque permet de multiplier, ça affiche le nombre d'heures/minutes/secondes travaillees
+
+puts "Et en secondes ?" # idem
+
+puts 10 * 5 * 11 * 60 * 60 # le resultat s'affiche : 1980000
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # la phrase s'affiche
+
+puts 3 + 2 < 5 - 7 #le resultat saffiche: c'est faux "false"
+
+puts "Ca fait combien 3 + 2 ? #{3 + 2}" #cela affiche 5
+puts "Ca fait combien 5 - 7 ? #{5 - 7}" #cela affiche 2
+
+puts "Ok, c'est faux alors !" #le texte est affiche
+
+puts "C'est drôle ça, faisons-en plus :" #idem
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #cela affiche true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5>= -2}" #cela affiche true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5<= -2}" #cela affiche false
