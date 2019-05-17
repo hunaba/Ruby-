96793f02ab4a549c4bf4bb73800e9e4b05758d7d
iku <abadeer.hunson@yandex.ru>
2019-05-17 12:25:35 +0200

    exo9 nom de famille qui rep

---
+++ TestRuby/exo09.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase? "
+user_name = gets.chomp 
+puts "Et ton nom de famille?"
+family_name = gets.chomp
+puts "Bonjour, #{user_name} + #{family_name} ! Est-ce que tu veux m'épouser? "
